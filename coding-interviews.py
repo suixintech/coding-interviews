9b2f39bf014be0a42f34248b9bff747a1f3ade1b
suixintech <48231568+suixintech@users.noreply.github.com>
2019-03-28 23:18:18 +0800

    Update coding-interviews.py

--- coding-interviews.py
+++ coding-interviews.py
@@ -355,3 +355,23 @@ class Solution12:
             return 1/eval('*'.join([str(base)] * abs(exponent)))
 
        
+#13、调整数组顺序使得奇数位于偶数前面的
+'''
+输入一个整数数组，实现一个函数来调整该数组中数字的顺序，
+使得所有的奇数位于数组的前半部分，所有的偶数位于数组的后半部分，
+并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+
+'''
+# -*- coding:utf-8 -*-
+class Solution13:
+    def reOrderArray(self, array):
+        # write code here
+        array1=[]
+        array2=[]
+        for val in array:
+            if val%2==0:
+                array2.append(val)
+            else:
+                array1.append(val)
+        array1.extend(array2)
+        return array1
